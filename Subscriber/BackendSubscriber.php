69406d09d830fc9db045fb72cafbceffbeb32545
tinect <s.koenig@tinect.de>
2018-07-03 12:44:38 +0200

    added missing subscriber for notification

---
+++ Subscriber/BackendSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TinectMailArchive\Subscriber;
+
+use Enlight\Event\SubscriberInterface;
+
+/**
+ * Class BackendSubscriber
+ * @package ShyimMailCatcher\Subscriber
+ */
+class BackendSubscriber implements SubscriberInterface
+{
+    /**
+     * @var string
+     */
+    private $viewDir;
+
+    public function __construct($viewDir)
+    {
+        $this->viewDir = $viewDir;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Enlight_Controller_Action_PostDispatch_Backend_Index' => 'onBackendIndex'
+        ];
+    }
+
+    /**
+     * @param \Enlight_Event_EventArgs $args
+     */
+    public function onBackendIndex(\Enlight_Event_EventArgs $args)
+    {
+        /** @var \Shopware_Controllers_Backend_Index $subject */
+        $subject = $args->getSubject();
+
+        if ($subject->Request()->getActionName() === 'index') {
+            $subject->View()->addTemplateDir($this->viewDir);
+            $subject->View()->extendsTemplate('backend/index/notifications.js');
+        }
+    }
+}
