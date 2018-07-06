bc090a96f39cd0958878cd150a38eb527b2b8830
tinect <s.koenig@tinect.de>
2018-07-06 12:17:40 +0200

    scheduleClearCache CACHE_LIST_DEFAULT

--- TinectMailArchive.php
+++ TinectMailArchive.php
@@ -14,9 +14,15 @@ use TinectMailArchive\Models\Mails;
 class TinectMailArchive extends Plugin
 {
 
+    public function activate(ActivateContext $context)
+    {
+        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
+    }
+
     public function update(UpdateContext $context)
     {
         $this->updateDatabase();
+        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
     }
 
     public function install(InstallContext $context)
